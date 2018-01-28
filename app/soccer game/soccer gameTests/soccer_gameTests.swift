749425572c0c75373aae8b9437db5baccad6a7b1
Mark Schwartz <mark610@mac-air-18.cs.iastate.edu>
2018-01-28 14:18:15 -0600

    Created initial Xcode project as team

---
+++ app/soccer game/soccer gameTests/soccer_gameTests.swift
@@ -0,0 +1,36 @@
+//
+//  soccer_gameTests.swift
+//  soccer gameTests
+//
+//  Created by Mark Schwartz on 1/28/18.
+//  Copyright © 2018 MG 6. All rights reserved.
+//
+
+import XCTest
+@testable import soccer_game
+
+class soccer_gameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
