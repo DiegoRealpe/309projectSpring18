d94bccd6e281c83a19a21813727557f9607eba9c
rtoepfer <ryantpfr@gmail.com>
2018-02-07 18:48:12 -0600

    Joystick moves box on screen

--- app/soccer game/soccer game/game scene/GameScene.swift
+++ app/soccer game/soccer game/game scene/GameScene.swift
@@ -16,14 +16,19 @@ class GameScene: SKScene {
     private var label : SKLabelNode?
     private var backLabel : SKLabelNode?
     private var joyStick : JoyStick?
+    private var playerNode : SKSpriteNode?
+    
+    
+    let movementSpeed = 100.0
     
     override func didMove(to view: SKView) {
         
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        fadeInMainLabel()
         self.backLabel = self.childNode(withName: "Back Label") as? SKLabelNode
+        self.playerNode = self.childNode(withName:"Player Node") as? SKSpriteNode
         
+        fadeInMainLabel()
         
         self.joyStick = JoyStick(parent: self, radius: 50.0, startPoint: CGPoint(x: 0, y: 0))
     }
@@ -39,7 +44,6 @@ class GameScene: SKScene {
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        
         
     }
     
@@ -63,10 +67,16 @@ class GameScene: SKScene {
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        self.joyStick?.acceptTouchMoved(touches: touches)
-        let str = self.joyStick!.getDebugMessage()
-        if let label = self.label {
-            label.text = str
+        if let js = self.joyStick{
+            js.acceptTouchMoved(touches: touches)
+            let str = js.getDebugMessage()
+            if let label = self.label {
+                label.text = str
+            }
+            let dx = js.xDirection * movementSpeed
+            let dy = js.yDirection * movementSpeed
+            self.playerNode?.physicsBody?.velocity = CGVector(dx: dx, dy: dy)
+            
         }
         
         
