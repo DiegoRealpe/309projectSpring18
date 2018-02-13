69d7f8c83010a499779c2d0da6f05ad6b49df591
rtoepfer <ryantpfr@gmail.com>
2018-02-13 10:17:02 -0600

    minor physics improvements and new comm testing support

---
+++ app/soccer game/soccer game/TCPResultExtension.swift
@@ -0,0 +1,25 @@
+//
+//  TCPResultExtension.swift
+//  soccer game
+//
+//  Created by rtoepfer on 2/12/18.
+//  Copyright © 2018 MG 6. All rights reserved.
+//
+
+import Foundation
+import SwiftSocket
+
+extension Result{
+    
+    func logError() {
+        logError(message : "there was a socket error:")
+    }
+    
+    func logError(message : String) {
+        if self.isFailure, let err = self.error{
+            print(message)
+            print(err.localizedDescription)
+        }
+    }
+    
+}
--- app/soccer game/soccer game/communcations/CommTestScreen.swift
+++ app/soccer game/soccer game/communcations/CommTestScreen.swift
@@ -8,21 +8,74 @@
 
 import SpriteKit
 import Alamofire
+import SwiftSocket
 
 //for a screen to test network communications in an enviroment with no reprecussions, will not be in final producrt
 class CommTestScreen: SKScene {
     
     //label for output
-    private var label:SKLabelNode?
+    private var debugLabel:SKLabelNode?
+    private var sendTCPLabel:SKLabelNode?
+    
+    //private var client:TCPClient?
     
     //sandbox here
     override func didMove(to view: SKView) {
         
         print("at comm test screen")
         
-        label = self.childNode(withName: "Debug Label") as? SKLabelNode
+        self.debugLabel = self.childNode(withName: "Debug Label") as? SKLabelNode
+        self.sendTCPLabel = self.childNode(withName: "Send Tcp") as? SKLabelNode
         
-        testHttp() //use refer string response to recievedResponse
+        
+        
+        //testHttp() //use refer string response to recievedResponse
+    }
+    
+    private func startTCPClient(){
+        //self.client = TCPClient(address: "localhost", port: 7234)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first, let tcpLabel = self.sendTCPLabel{
+            if(tcpLabel.contains(touch.location(in: self))){
+                sendTcp()
+            }
+        }
+    }
+    
+    func sendTcp(){
+        let client = TCPClient(address: "localhost", port: 7234)
+        client.connect(timeout: 2).logError()
+        //client.send(string: "hello server\n").logError()
+
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
+            if let recieved = client.read(50){
+                print(String(bytes: recieved, encoding: .utf8)!)
+                client.send(string: "ryan ").logError()
+                self.step2(client: client)
+            }
+        })
+        
+    }
+    
+    func step2(client : TCPClient){
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
+            if let recieved = client.read(100){
+                print(String(bytes: recieved, encoding: .utf8)!)
+                
+                //client.send(string : ":quit").logError()
+                client.close()
+            }
+        })
+        
+        
+    }
+    
+    func tcpLogic(client : TCPClient){
+        
     }
     
     fileprivate func testHttp(){
@@ -37,7 +90,7 @@ class CommTestScreen: SKScene {
         if let data:Data = response.data, let str:String = String(data: data, encoding: .utf8){
             print("got response " + str)
             
-            self.label!.text = str
+            self.debugLabel!.text = str
         }
     }
     
