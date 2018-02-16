b94f6dd84313166097d8e5e2dc19fd13a65b0fa2
Mark Schwartz <mark610@mac-air-18.cs.iastate.edu>
2018-02-16 11:33:53 -0600

    Added sks file

--- app/soccer game/soccer game/game scene/GameScene.swift
+++ app/soccer game/soccer game/game scene/GameScene.swift
@@ -95,11 +95,15 @@ class GameScene: SKScene {
             let dy = js.yDirection * movementSpeed
             self.playerNode?.physicsBody?.velocity = CGVector(dx: dx, dy: dy)
             
+            
+            
         }
+        
         
         
         for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
+    
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
