daef7fea59167a41abf8d5f06fb225f02b0792cd
rtoepfer <ryantpfr@gmail.com>
2018-02-19 11:20:52 -0600

    refactored some blocks into cleaner functions

--- server/server.go
+++ server/server.go
@@ -55,53 +55,52 @@ func main() {
 
 	connPasser = make(chan net.Conn)
 
-	go func() {//handle http requests
-		http.HandleFunc("/", handler)
-		err := http.ListenAndServe(":80", nil)
-
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-	}()
+	go startHttpServer()
 
 	i := 0
 
-	for connected := range connPasser {//when a player connects, initialize their readers and writers
-		
-		g.connections[i] = connected
-		g.reader[i] = bufio.NewReader(g.connections[i])
-		g.writer[i] = bufio.NewWriter(g.connections[i])
-		hellobyte := []byte{byte(122), byte(i)}
-		g.writer[i].Write(hellobyte)
-		g.writer[i].Flush()
-		
-		time.Sleep(2*time.Second)
-		testpacket := ServerPacket{//testing; to be removed later
-			serverPlayerState: 121,
-			playernumber: uint8(i),
-			xPosition: 0,
-			yPosition: 0,
-			xVelocity: 0,
-			yVelocity: 0,
-			timestamp: 0,
-		}
-		
-		testbytes := ParseServerPacket(testpacket)
-		
-		g.writer[i].Write(testbytes)
-		g.writer[i].Flush()
-		
-
-
-		
+	//when a player connects, initialize their readers and writers
+	for connected := range connPasser {
+		initializeConnection(g, i, connected)
 		i++
-		if i > 1 {break}
 	}
+
 	ListenAndSend(g, 0)
 	ListenAndSend(g, 1)
 
-	for{}
-	
+	for {}
+}
+
+
+func initializeConnection(g Game, playerNumber int, connection net.Conn) {
+	g.connections[playerNumber] = connection
+	g.reader[playerNumber] = bufio.NewReader(g.connections[playerNumber])
+	g.writer[playerNumber] = bufio.NewWriter(g.connections[playerNumber])
+	hellobyte := []byte{byte(122), byte(playerNumber)}
+	g.writer[playerNumber].Write(hellobyte)
+	g.writer[playerNumber].Flush()
+	time.Sleep(2 * time.Second)
+	testpacket := ServerPacket{//testing; to be removed later
+		serverPlayerState: 121,
+		playernumber: uint8(playerNumber),
+		xPosition: 0,
+		yPosition: 0,
+		xVelocity: 0,
+		yVelocity: 0,
+		timestamp: 0,
+	}
+	testbytes := ParseServerPacket(testpacket)
+	g.writer[playerNumber].Write(testbytes)
+	g.writer[playerNumber].Flush()
+}
+
+func startHttpServer() {
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":80", nil)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
 
 
