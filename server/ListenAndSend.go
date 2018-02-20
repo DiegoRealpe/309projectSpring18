ab135099aa854283d14c9877251f7ceebdc8c89e
njkim <njkim@iastate.edu>
2018-02-20 14:35:18 -0600

    no longer says fuck when recieving data

--- server/ListenAndSend.go
+++ server/ListenAndSend.go
@@ -11,41 +11,41 @@ func ListenAndSend(g Game, connNumber int) {
 	go func(){
 		for {
 			fmt.Println("reading from connection", connNumber)
-				readbyte, _ := g.reader[connNumber].Peek(1)
-				id := int(readbyte[0])
-				fmt.Println("fuck")
-				if id == 120 {
-					bytemessage, err := g.reader[connNumber].Peek(17)
-					if err != nil {
-						fmt.Println("packet error: " + err.Error())
-						g.reader[connNumber].ReadByte()
-					} else {
-						//parse the byte message
-						rcvpacket := ParseBytes(bytemessage)
-						fmt.Println(rcvpacket.clientPlayerState, rcvpacket.xPosition, rcvpacket.yPosition)
-						//construct a message to broadcast to the clients
-						sendpacket := ServerPacket{
-							serverPlayerState: 121,
-							playernumber:      uint8(connNumber),
-							xPosition:         rcvpacket.xPosition,
-							yPosition:         rcvpacket.yPosition,
-							xVelocity:         rcvpacket.xVelocity,
-							yVelocity:         rcvpacket.yVelocity,
-							timestamp:         0,
-						}
-						
-						sendbytes := ParseServerPacket(sendpacket)
-						
-						//broadcast that message to the clients
-						for _, reciever := range g.writer {
-							reciever.Write(sendbytes)
-						}
-						
-					}
-					g.reader[connNumber].Read(bytemessage)
+			readbyte, err := g.reader[connNumber].Peek(1)
+			id := int(readbyte[0])
+			fmt.Println("got it fam")
+			if id == 120 {
+				bytemessage, err := g.reader[connNumber].Peek(17)
+				if err != nil {
+					fmt.Println("packet error: " + err.Error())
+					g.reader[connNumber].ReadByte()
 				} else {
-					g.reader[connNumber].Discard(1)
+					//parse the byte message
+					rcvpacket := ParseBytes(bytemessage)
+					fmt.Println(rcvpacket.clientPlayerState, rcvpacket.xPosition, rcvpacket.yPosition)
+					//construct a message to broadcast to the clients
+					sendpacket := ServerPacket{
+						serverPlayerState: 121,
+						playernumber:      uint8(connNumber),
+						xPosition:         rcvpacket.xPosition,
+						yPosition:         rcvpacket.yPosition,
+						xVelocity:         rcvpacket.xVelocity,
+						yVelocity:         rcvpacket.yVelocity,
+						timestamp:         0,
+					}
+					
+					sendbytes := ParseServerPacket(sendpacket)
+					
+					//broadcast that message to the clients
+					for _, reciever := range g.writer {
+						reciever.Write(sendbytes)
+					}
+					
 				}
+				g.reader[connNumber].Read(bytemessage)
+			} else {
+				g.reader[connNumber].Discard(1)
+			}
 		}
 	}()
 }
