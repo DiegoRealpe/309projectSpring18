41c7b6a93a4e466273444c82cba21ba58400ca42
Mark <mark610@iastate.edu>
2018-02-24 13:45:29 -0600

    Started Facebook Login

--- app/soccer game/soccer game/Accounts/NewAccountOrLogin.swift
+++ app/soccer game/soccer game/Accounts/NewAccountOrLogin.swift
@@ -6,10 +6,14 @@
 //  Copyright © 2018 MG 6. All rights reserved.
 //
 import SpriteKit
+import UIKit
+
+import FBSDKLoginKit
 //import Alamofire
 
 class NewAccountOrLogin: SKScene{
     
+    var dict : [String : AnyObject]!
     var back : SKNode?
     var login: SKNode?
     
@@ -40,7 +44,7 @@ class NewAccountOrLogin: SKScene{
             }
             else if login.contains(point)
             {
-                moveToLogin()
+                loginButtonClicked()
             }
         }
         
@@ -53,15 +57,34 @@ class NewAccountOrLogin: SKScene{
         }
     }
     
-    
-    func moveToLogin(){
-        
-        //let storyboard = UIStoryboard(name: "Login", bundle: UIApplication)
-        //let vc = storyboard.instantiateViewController(withIdentifier: "Login") as UIViewController
-        //vc.present(vc, animated: true, completion: nil)
-
-        
+    @objc func loginButtonClicked() {
+        let loginManager = LoginManager()
+        loginManager.logIn([ .publicProfile ], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                self.getFBUserData()
+            }
+        }
     }
+    
+    //function is fetching the user data
+    func getFBUserData(){
+        if((FBSDKAccessToken.current()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    self.dict = result as! [String : AnyObject]
+                    print(result!)
+                    print(self.dict)
+                }
+            })
+        }
+    }
+    
+
     
 }
 
