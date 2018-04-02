46b481f9750887675ae3722d2d04902e843ae3a2
rtoepfer <ryantpfr@gmail.com>
2018-04-02 11:35:08 -0500

    working on it

---
+++ app/soccer game/soccer game/lobby scene/ChatView.swift
@@ -0,0 +1,21 @@
+//
+//  ChatView.swift
+//  soccer game
+//
+//  Created by rtoepfer on 4/2/18.
+//  Copyright © 2018 MG 6. All rights reserved.
+//
+
+import UIKit
+
+class ChatView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
