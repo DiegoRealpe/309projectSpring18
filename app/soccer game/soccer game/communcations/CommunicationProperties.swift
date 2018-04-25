33d67ddcb8a085430405f46dd6001635fc86bf1f
rtoepfer <rtoepfer@iastate.edu>
2018-04-25 12:55:10 -0500

    set all comms to local ports

--- app/soccer game/soccer game/communcations/CommunicationProperties.swift
+++ app/soccer game/soccer game/communcations/CommunicationProperties.swift
@@ -10,12 +10,12 @@ import Foundation
 
 
 struct CommunicationProperties{
-    static let gameServiceHost = "proj-309-MG-6.cs.iastate.edu"
-    //static let gameServiceHost = "localhost"
+    //static let gameServiceHost = "proj-309-MG-6.cs.iastate.edu"
+    static let gameServiceHost = "localhost"
     static let gameServiceHttpPort = 6000
     
     
-    static let crudServiceHost = "proj-309-MG-6.cs.iastate.edu"
-    //static let crudServiceHost = "localhost"
+    //static let crudServiceHost = "proj-309-MG-6.cs.iastate.edu"
+    static let crudServiceHost = "localhost"
     static let crudServicePort = 8000
 }
